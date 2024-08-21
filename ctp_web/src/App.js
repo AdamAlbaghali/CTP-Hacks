c449f5eb66f433ea722ba1c39af281054250e720
Pretam Chowdhury <Chowdhurypretam@gmail.com>
2024-08-21 15:17:38 -0400

    New commit

--- ctp_web/src/App.js
+++ ctp_web/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Survey from './Survey';
+import Discussion from './Discussion';
+import AboutUs from './AboutUs';
+import Home from './Home'; // Make sure you create a Home component or adjust the import as necessary
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey" element={<Survey />} />
+        <Route path="/discussion" element={<Discussion />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        {/* Redirect to home if route is not found */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
