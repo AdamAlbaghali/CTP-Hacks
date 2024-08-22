3b9293680c94b18426ba1f09977b6bbaa75d6fe6
Carlo Ace Sagad <carloace166@gmail.com>
2024-08-22 16:05:22 -0400

    fixed code format

--- ctp_web/src/Discussion.js
+++ ctp_web/src/Discussion.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 function Discussion() {
   return (
@@ -67,56 +67,64 @@ function Discussion() {
       </section>
 
       <section id="title">
-      <div className="px-4 pt-5 my-5 text-center border-bottom">
+        <div className="px-4 pt-5 my-5 text-center border-bottom">
           <h1 className="display-4 fw-bold text-body-emphasis">
             CUNY Student Needs
           </h1>
         </div>
       </section>
 
-
       {/* Body */}
       <section id="Body-1">
-      <div className="container col-xxl-8 px-4 custom-border-bottom">
-        <div className="row flex-lg-row-reverse align-items-center g-5 py-5 rounded-circle">
-          <div className="col-10 col-sm-8 col-lg-6">
-            <img
-              src="grouppic.jpeg"
-              className="-block mx-lg-auto img-fluid rounded-circle ms-lg-0 mr-0 custom-image-2"
-              alt="Profile"
-              width="350"
-              loading="lazy"
-            />
-          </div>
-          <div className="col-lg-6">
-            <p className="lead">
-            Our project is dedicated to understanding the needs of CUNY students across various campuses. Inspired by the findings from a study focused on students in the Bronx, which highlighted significant challenges such as food insecurity, housing instability, lack of mental health treatment, and inadequate healthcare access, we aim to replicate this research across other CUNY campuses.
-            </p>
-          </div>
-        </div>
-      </div>
-    
-    </section>
-
-          <section id="Body-2">
-    <div className="container col-xxl-8 px-4 custom-border-bottom">
-        <div className="row align-items-center g-5 py-5">
-          <div className="col-10 col-sm-8 col-lg-6 align-self-start">
-            <img
-              src="fivepic.jpeg"
-              className="d-block mx-lg-auto img-fluid rounded-circle ms-lg-0 mr-0 custom-image"
-              alt="Profile"
-              width="350"
-              loading="lazy"
-            />
-          </div>
-          <div className="col-lg-6">
-            <p className="lead">
-            By gathering this data, we hope to identify areas where resources are most needed and advocate for enhanced support for all CUNY students. Our research will focus on critical areas such as food insecurity, housing instability, mental health services, and healthcare access, ensuring that every student has the resources they need to succeed.
-            </p>
+        <div className="container col-xxl-8 px-4 custom-border-bottom">
+          <div className="row flex-lg-row-reverse align-items-center g-5 py-5 rounded-circle">
+            <div className="col-10 col-sm-8 col-lg-6">
+              <img
+                src="grouppic.jpeg"
+                className="-block mx-lg-auto img-fluid rounded-circle ms-lg-0 mr-0 custom-image-2"
+                alt="Profile"
+                width="350"
+                loading="lazy"
+              />
+            </div>
+            <div className="col-lg-6">
+              <p className="lead">
+                Our project is dedicated to understanding the needs of CUNY
+                students across various campuses. Inspired by the findings from
+                a study focused on students in the Bronx, which highlighted
+                significant challenges such as food insecurity, housing
+                instability, lack of mental health treatment, and inadequate
+                healthcare access, we aim to replicate this research across
+                other CUNY campuses.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section id="Body-2">
+        <div className="container col-xxl-8 px-4 custom-border-bottom">
+          <div className="row align-items-center g-5 py-5">
+            <div className="col-10 col-sm-8 col-lg-6 align-self-start">
+              <img
+                src="fivepic.jpeg"
+                className="d-block mx-lg-auto img-fluid rounded-circle ms-lg-0 mr-0 custom-image"
+                alt="Profile"
+                width="350"
+                loading="lazy"
+              />
+            </div>
+            <div className="col-lg-6">
+              <p className="lead">
+                By gathering this data, we hope to identify areas where
+                resources are most needed and advocate for enhanced support for
+                all CUNY students. Our research will focus on critical areas
+                such as food insecurity, housing instability, mental health
+                services, and healthcare access, ensuring that every student has
+                the resources they need to succeed.
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
 
       {/* Footer - will probably remove later */}
@@ -130,12 +138,18 @@ function Discussion() {
                 </a>
               </li>
               <li className="nav-item">
-                <a href="/discussion" className="nav-link px-2 text-body-secondary">
+                <a
+                  href="/discussion"
+                  className="nav-link px-2 text-body-secondary"
+                >
                   Discussion
                 </a>
               </li>
               <li className="nav-item">
-                <a href="/aboutus" className="nav-link px-2 text-body-secondary">
+                <a
+                  href="/aboutus"
+                  className="nav-link px-2 text-body-secondary"
+                >
                   About
                 </a>
               </li>
